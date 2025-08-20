8c328f75a06599e3a2d4022cfd0e9f7a52ba1ea8
Martin Fenner <martin@front-matter.io>
2025-08-20 21:15:09 +0200

    load only one default locale and style

--- kcite-citeproc/kcite.js
+++ kcite-citeproc/kcite.js
@@ -259,179 +259,11 @@ var kcite_locales = {
 </locale>`,
 };
 var kcite_styles = {
-  nature: `<?xml version="1.0" encoding="utf-8"?>
-<style xmlns="http://purl.org/net/xbiblio/csl" class="in-text" version="1.0" demote-non-dropping-particle="sort-only" default-locale="en-GB">
-  <info>
-    <title>Nature</title>
-    <id>http://www.zotero.org/styles/nature</id>
-    <link href="http://www.zotero.org/styles/nature" rel="self"/>
-    <link href="http://www.nature.com/nature/authors/gta/index.html#a5.4" rel="documentation"/>
-    <link href="http://www.nature.com/srep/publish/guidelines#references" rel="documentation"/>
-    <author>
-      <name>Michael Berkowitz</name>
-      <email>mberkowi@gmu.edu</email>
-    </author>
-    <category citation-format="numeric"/>
-    <category field="science"/>
-    <category field="generic-base"/>
-    <issn>0028-0836</issn>
-    <eissn>1476-4687</eissn>
-    <updated>2022-07-01T16:03:34+00:00</updated>
-    <rights license="http://creativecommons.org/licenses/by-sa/3.0/">This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 License</rights>
-  </info>
-  <macro name="title">
-    <choose>
-      <if type="bill book graphic legal_case legislation motion_picture report song" match="any">
-        <text variable="title" font-style="italic" text-case="title"/>
-      </if>
-      <else>
-        <text variable="title"/>
-      </else>
-    </choose>
-  </macro>
-  <macro name="author">
-    <names variable="author">
-      <name sort-separator=", " delimiter=", " and="symbol" initialize-with=". " delimiter-precedes-last="never" name-as-sort-order="all"/>
-      <label form="short" prefix=", "/>
-      <et-al font-style="italic"/>
-    </names>
-  </macro>
-  <macro name="access">
-    <choose>
-      <if variable="volume" type="article dataset software" match="any"/>
-      <else-if variable="DOI">
-        <text variable="DOI" prefix="doi:"/>
-      </else-if>
-    </choose>
-  </macro>
-  <macro name="access-data">
-    <choose>
-      <if type="dataset software" match="any">
-        <text variable="DOI" prefix="https://doi.org/"/>
-      </if>
-    </choose>
-  </macro>
-  <macro name="issuance">
-    <choose>
-      <if type="bill book graphic legal_case legislation motion_picture song thesis chapter paper-conference" match="any">
-        <group delimiter="; " suffix=".">
-          <group delimiter=", " prefix="(" suffix=")">
-            <text variable="publisher" form="long"/>
-            <text variable="publisher-place"/>
-            <date variable="issued">
-              <date-part name="year"/>
-            </date>
-          </group>
-        </group>
-      </if>
-      <else-if type="article">
-        <group delimiter=" ">
-          <choose>
-            <if variable="genre" match="any">
-              <text variable="genre" text-case="capitalize-first"/>
-            </if>
-            <else>
-              <text term="article" text-case="capitalize-first"/>
-            </else>
-          </choose>
-          <text term="at"/>
-          <choose>
-            <if variable="DOI" match="any">
-              <text variable="DOI" prefix="https://doi.org/"/>
-            </if>
-            <else>
-              <text variable="URL"/>
-            </else>
-          </choose>
-          <date date-parts="year" form="text" variable="issued" prefix="(" suffix=")"/>
-        </group>
-      </else-if>
-      <else-if type="dataset software" match="any">
-        <group delimiter=" ">
-          <text variable="publisher"/>
-          <text macro="access-data"/>
-          <date date-parts="year" form="text" variable="issued" prefix="(" suffix=")"/>
-        </group>
-      </else-if>
-      <else-if type="report webpage post post-weblog" match="any">
-        <group delimiter=" ">
-          <text variable="URL"/>
-          <date date-parts="year" form="text" variable="issued" prefix="(" suffix=")"/>
-        </group>
-      </else-if>
-      <else>
-        <date variable="issued" prefix="(" suffix=")">
-          <date-part name="year"/>
-        </date>
-      </else>
-    </choose>
-  </macro>
-  <macro name="container-title">
-    <choose>
-      <if type="article-journal">
-        <text variable="container-title" font-style="italic" form="short"/>
-      </if>
-      <else>
-        <text variable="container-title" font-style="italic"/>
-      </else>
-    </choose>
-  </macro>
-  <macro name="editor">
-    <choose>
-      <if type="chapter paper-conference" match="any">
-        <names variable="editor" prefix="(" suffix=")">
-          <label form="short" suffix=" "/>
-          <name and="symbol" delimiter-precedes-last="never" initialize-with=". " name-as-sort-order="all"/>
-        </names>
-      </if>
-    </choose>
-  </macro>
-  <macro name="volume">
-    <choose>
-      <if type="article-journal" match="any">
-        <text variable="volume" font-weight="bold" suffix=","/>
-      </if>
-      <else>
-        <group delimiter=" ">
-          <label variable="volume" form="short"/>
-          <text variable="volume"/>
-        </group>
-      </else>
-    </choose>
-  </macro>
-  <citation collapse="citation-number">
-    <sort>
-      <key variable="citation-number"/>
-    </sort>
-    <layout vertical-align="sup" delimiter=",">
-      <text variable="citation-number"/>
-    </layout>
-  </citation>
-  <bibliography et-al-min="6" et-al-use-first="1" second-field-align="flush" entry-spacing="0" line-spacing="2">
-    <layout suffix=".">
-      <text variable="citation-number" suffix="."/>
-      <group delimiter=" ">
-        <text macro="author" suffix="."/>
-        <text macro="title" suffix="."/>
-        <choose>
-          <if type="chapter paper-conference" match="any">
-            <text term="in"/>
-          </if>
-        </choose>
-        <text macro="container-title"/>
-        <text macro="editor"/>
-        <text macro="volume"/>
-        <text variable="page"/>
-        <text macro="issuance"/>
-        <text macro="access"/>
-      </group>
-    </layout>
-  </bibliography>
-</style>`,
+  apa: null, // APA style
 };
 
 // Default settings - will be overridden by WordPress settings if available
-var kcite_default_style = "nature";
+var kcite_default_style = "apa";
 var kcite_default_locale = "en-US";
 
 // Load WordPress settings if available
@@ -604,14 +436,14 @@ function loadCSLStyleSync(styleName, fileName) {
 // Function to get available styles
 function getAvailableStyles() {
   return {
-    "american-chemical-society": "ACS",
     apa: "APA",
-    "chicago-author-date": "Chicago Manual of Style (Author-Date)",
-    "harvard-cite-them-right": "Harvard",
-    ieee: "IEEE",
-    "modern-language-association": "MLA",
-    nature: "Nature",
-    vancouver: "Vancouver",
+    // "american-chemical-society": "ACS",
+    // "chicago-author-date": "Chicago Manual of Style (Author-Date)",
+    // "harvard-cite-them-right": "Harvard",
+    // ieee: "IEEE",
+    // "modern-language-association": "MLA",
+    // nature: "Nature",
+    // vancouver: "Vancouver",
   };
 }
 
@@ -702,14 +534,14 @@ async function loadCSLStyleFromFile(styleName, filePath) {
 // Function to load multiple CSL files
 async function loadAllCSLStyles() {
   const stylesToLoad = [
-    "american-chemical-society",
     "apa",
-    "chicago-author-date",
-    "harvard-cite-them-right",
-    "ieee",
-    "modern-language-association",
-    "nature",
-    "vancouver",
+    // "american-chemical-society",
+    // "chicago-author-date",
+    // "harvard-cite-them-right",
+    // "ieee",
+    // "modern-language-association",
+    // "nature",
+    // "vancouver",
   ];
 
   for (const styleName of stylesToLoad) {
@@ -722,18 +554,18 @@ async function loadAllCSLStyles() {
   }
 }
 
-// Function to preload all available locales
+// Function to preload some available locales
 async function loadAllLocalesAsync() {
   const localesToLoad = [
-    "de-DE",
-    "en-GB",
     "en-US",
-    "es-ES",
-    "fr-FR",
-    "it-IT",
-    "ja-JP",
-    "pt-BR",
-    "zh-CN",
+    // "de-DE",
+    // "en-GB",
+    // "es-ES",
+    // "fr-FR",
+    // "it-IT",
+    // "ja-JP",
+    // "pt-BR",
+    // "zh-CN",
   ];
 
   for (const localeName of localesToLoad) {
@@ -750,11 +582,11 @@ async function loadAllLocalesAsync() {
 if (typeof window !== "undefined") {
   window.addEventListener("load", async function () {
     try {
-      // Load styles and locales in parallel
+      // Load default locale and style in parallel
       await Promise.all([loadAllCSLStyles(), loadAllLocalesAsync()]);
-      console.log("All styles and locales loaded successfully");
+      console.log("Default locale and style loaded successfully");
     } catch (error) {
-      console.error("Error loading styles or locales:", error);
+      console.error("Error loading default locale or style:", error);
     }
   });
 }
