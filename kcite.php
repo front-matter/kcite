56d7f42c29088c83929c95cb114d7c920acdb4de
Martin Fenner <martin@front-matter.io>
2025-08-21 07:46:52 +0200

    make vancouver the default style

--- kcite.php
+++ kcite.php
@@ -3,7 +3,7 @@
    Plugin Name: Kcite
    Plugin URI: http://knowledgeblog.org/kcite-plugin
    Description: Add references and bibliography to blogposts
-   Version: 1.7.42
+   Version: 1.7.43
    Author: Simon Cockell, Phillip Lord, Martin Fenner
    Author URI: http://knowledgeblog.org
    Email: knowledgeblog@googlegroups.com
